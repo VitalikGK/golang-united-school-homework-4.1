1f327481ab2843078b378e490be9c9980164387c
vital <vitalikgk@mail.ru>
2022-05-13 09:50:32 +0400

    test

--- reverse_string.go
+++ reverse_string.go
@@ -1,6 +1,21 @@
 package reverse_string
 
+//import "fmt"
+
 func ReverseString(input string) (output string) {
-	// solution goes here
+	n := 0
+	rune := make([]rune, len(input))
+	for _, r := range input {
+		rune[n] = r
+		n++
+	}
+	rune = rune[0:n]
+	// Reverse
+	for i := 0; i < n/2; i++ {
+		rune[i], rune[n-1-i] = rune[n-1-i], rune[i]
+	}
+	// Convert back to UTF-8.
+	output = string(rune)
+	//fmt.Println(output)
 	return output
 }
